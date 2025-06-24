c5c094b18d13b72b4aec9af435f4ece5ce624e2c
Jocelino Neto <netoj96@live.com>
2025-06-24 20:29:55 -0300

    mariokart.js with common js modules

---
+++ mariokart-simulator/src/models/players.js
@@ -0,0 +1,57 @@
+const characteres = [
+    {
+        name: 'Mario',
+        icon: '🍄🔴',
+        velocity: 4, // velocidade
+        moviment: 3, // manobrabilidade
+        power: 3, // poder
+        points: 0,
+    },
+
+    {
+        name: 'Luigi',
+        icon: '🍄🟢',
+        velocity: 3, // velocidade
+        moviment: 4, // manobrabilidade
+        power: 4, // poder
+        points: 0,
+    },    
+    
+    {
+        name: 'Peach',
+        icon: '🍑',
+        velocity: 3,
+        moviment: 4,
+        power: 2,
+        points: 0,
+    },
+
+    {
+        name: 'Yoshi',
+        icon: '🦖',
+        velocity: 2,
+        moviment: 4,
+        power: 3,
+        points: 0,
+    },
+
+    {
+        name: 'Bowser',
+        icon: '🦔',
+        velocity: 5,
+        moviment: 2,
+        power: 5,
+        points: 0,
+    },
+
+    {
+        name: 'Donkey Kong',
+        icon: '🐒',
+        velocity: 2,
+        moviment: 2,
+        power: 5,
+        points: 0,
+    },
+];
+
+module.exports = characteres;
